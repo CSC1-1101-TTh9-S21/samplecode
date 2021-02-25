821a252b1015c2a8fa753ae1d55e82f14ddaecc0
emilypx <emilypx@rit.edu>
2021-02-24 20:50:23 -0500

    more code

---
+++ week4/while_and_for.py
@@ -0,0 +1,43 @@
+
+## Demonstrating how to do for in range loops with while
+
+
+# one arguments
+for i in range(10):
+    print(i, end=" ")
+print()
+
+i = 0
+while i < 10:
+    print(i, end=" ")
+    i += 1
+print()
+
+
+
+# two arguments
+for i in range(3, 10):
+    print(i, end=" ")
+print()
+
+i = 3
+while i < 10:
+    print(i, end=" ")
+    i += 1
+print()
+
+
+
+
+# three arguments
+for i in range(3,10,2):
+    print(i, end=" ")
+print()
+
+i = 3
+while i < 10:
+    print(i, end=" ")
+    i += 2
+print()
+
+
